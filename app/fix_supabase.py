3d052aa643dbf9ea7ca9939928ffcb2684952c18
ERROR_404 <juliogabryel.jas@gmail.com>
2026-04-21 10:04:27 -0300

    exl

--- app/fix_supabase.py
+++
@@ -1,9 +0,0 @@
-with open('/home/julio/Codigos/Codigo-6X2/app/src/pages/supabase.py', 'r') as f:
-    content = f.read()
-
-content = content.replace('import requests', "import requests\nimport urllib3\nurllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)")
-
-content = content.replace('timeout=1000', 'timeout=1000,\n            verify=False')
-
-with open('/home/julio/Codigos/Codigo-6X2/app/src/pages/supabase.py', 'w') as f:
-    f.write(content)
--- app/test_supabase_import.py
+++
@@ -1,7 +0,0 @@
-import sys
-import os
-sys.path.append(os.path.join(os.getcwd(), 'src'))
-from pages.supabase import ler_tabela
-
-res = ler_tabela('login')
-print("Result:", res)
